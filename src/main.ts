336967d320db60f92b42662e1bd407ecf1ce4909
Ashwani Yadav <49113451+Ashwani-Ydv@users.noreply.github.com>
2023-10-20 22:45:01 +0530

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(3000);
-// }
-// bootstrap();
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors();
-//   await app.listen(3000);
-// }
-// bootstrap();
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Replace with your allowed origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Set this to true if you need to allow credentials (cookies, authorization headers, etc.)
+  });
   await app.listen(3000);
+ 
 }
 bootstrap();
